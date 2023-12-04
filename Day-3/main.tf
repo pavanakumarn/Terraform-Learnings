484638b96fda11295c6040c9ad64956219845122
pavanakumarn <126378290+pavanakumarn@users.noreply.github.com>
2023-12-04 10:15:22 +0000

    saving2

---
+++ Day-3/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "ec2_instance" {
+  source = "./modules/ec2_instance"
+  ami_value = "ami-0287a05f0ef0e9d9a"
+  instance_type_value = "t2.micro"
+  subnet_id_value = "subnet-033d20539fc19bc4b"
+  key_name_value = "INFY"
+
+}
